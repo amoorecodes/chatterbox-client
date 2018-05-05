f5e0c72414ed9a669db976e9727dbfef8c2ada68
unfaceit <ryabtsev.art@gmail.com>
2018-05-04 20:22:39 -0700

    It's alive!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,21 +22,14 @@ let app = {
   myFriends: {},
   messages: [],
   chatrooms: {},
+  currentUser: window.location.search.split("=")[1],
+  currentRoom: $("#roomSelect option:checked").val() || "lobby"
 };
 
+//need to create a default state of the room 
+
 app.send = function(message) {
-  
-  // have a listener on input
-    // on click, it will create a message obj
-      // 
-  
-  // let message = {
-  //   username: null,
-  //   text: null,
-  //   roomname: null
-     
-  // }
-  
+
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
   url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
@@ -55,6 +48,10 @@ app.send = function(message) {
 
 app.init = function() {
   app.fetch();
+  app.renderRoom("Lobby");
+  app.renderRoom("hotel")
+  app.renderRoom("place")
+  // app.username = $()
 };
 
 app.fetch = function() {
@@ -62,9 +59,10 @@ app.fetch = function() {
   // This is the url you should use to communicate with the parse API server.
   url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
   type: 'GET',
-  // data: JSON.stringify.apply(this),
+  data: {order: "-createdAt"},
   contentType: 'application/json',
   success: function (data) {
+    app.clearMessages();
     data.results.forEach(message => app.renderMessage(message));
     console.log("data:", data)
   },
@@ -81,30 +79,59 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  $(`<div><a href="#" class="username">${message.username}</a><span>${message.text}</span></div>`).appendTo('#chats');
+  var div = $('<div></div>')
+  var a = $(`<a href="#" class="username">${message.username}</a>`)
+  a.on('click', app.handleUsernameClick);
+  var b = $(`<span>${message.text}</span>`);
+  div.append(a).append(b);
+  $('#chats').append(div);
+  // $(`<span>${message.text}</span>`).appendTo('#chats');
+  // $(`<div><a href="#" class="username">${message.username}</a><span>${message.text}</span></div>`).appendTo('#chats');
   // $('#chats').children().append(`<span>${message}</span>`);
 }
 
 app.renderRoom = function(room) {
-    $(`<div>${room}</div>`).appendTo('#roomSelect');
+    this.chatrooms[room] = false;
+    $(`<a href="#">${room}</a>`).appendTo('#roomSelect');
 }
 
 app.handleUsernameClick = function() {
-   $('body').on('click', 'a', function() {
-    console.log('e');
+  console.log(window.location.search.slice(10))
     app.myFriends[$(this).text()] = true;
-  });
 }
+
+app.handleSubmit = function(event) {
+  event.preventDefault();
+
+  var message = {
+    username: app.currentUser,
+    text: $('#text').val(),
+    room: app.currentRoom,
+  }
+
+  app.send(message);
+ // $("form").submit(app.send($(':input'))) 
+ // var regex = /[a-z0-9]/i regex.test(_____ pass what we want to test)
+
+}
+
+
 
 $(document).ready(function() {
   
   app.init();
+  setInterval(app.fetch, 2000);
+  $('.postBtn').on('click', app.handleSubmit);
    
-   $('body').on('click', 'a', function() {
-    console.log('e');
-    app.myFriends[$(this).text()] = true;
-  });
+  //  $('body').on('click', 'a', function() {
+  //   console.log('e');
+  //   app.myFriends[$(this).text()] = true;
+  // });
   
+  // $("#input").submit(app.handleSubmit($(':input')))
 })
 
+
+
+//$("#roomSelect").change()
 
